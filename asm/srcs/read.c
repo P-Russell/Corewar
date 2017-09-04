81c2b6824a506dd994d54ea1d0366c5d0d0189ed
Daniel BARROW <dbarrow@e4r6p2.wtc.co.za>
2017-09-04 10:38:44 +0200

    splitspace

--- asm/srcs/read.c
+++ asm/srcs/read.c
@@ -6,22 +6,66 @@
 /*   By: prussell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/28 09:52:40 by prussell          #+#    #+#             */
-/*   Updated: 2017/08/28 15:59:10 by prussell         ###   ########.fr       */
+/*   Updated: 2017/09/01 13:30:17 by dbarrow          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-int		get_encoding_byte(t_src_line *lines)
+int		get_encoding_byte(t_src_line **lines)
 {
 	int	i;
+	int	square;
+	int	n;
 
 	i = 0;
-	while (lines[i].data != NULL)
+	n = MAX_ARGS_NUMBER;
+	while (lines[i]->data != NULL)
 	{
+		lines[i]->acb = 0;
+		while (lines[i]->params[n])
+		{
+			lines[i]->acb += lines[i]->params[n] * (square * square);
+			square /= 2;
+			n--;
+		}
 		i++;
 	}
 	return (EXIT_SUCCESS);
+}
+
+int		get_params(t_src_line **lines)
+{
+	int		i;
+	int		n;
+	int		p;
+	char	**split;
+
+	n = 0;
+	i = 0;
+	p = 0;
+	while (lines[i]->data != NULL)
+	{
+		split = ft_strplit(lines[i]->data, ' ');
+		if (ft_strstr(split[0], LABEL_CHAR) != NULL)
+		{
+			lines[i]->label = split[0];
+			n++;
+		}
+		n++;
+		while (split[n])
+		{
+			if (ft_strstr(split[n], "r") != NULL)
+				lines[i]->params[p] = 1;
+			else if (ft_strstr(split[n], "%") != NULL)
+				lines[i]->params[p] = 3;
+			else if (ft_isdigit(split[n]))
+				lines[i]->params[p] = 2;
+			else
+				lines[i]->params[p] = 0;
+		}
+		ft_arrdel(split);
+	}
 }
 
 int		get_raw_data(t_src_line *lines, int fd)
