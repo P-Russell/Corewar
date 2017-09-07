6b1dc1b8cfdae1b1e13ab2d00586752a02b61272
Patrick RUSSELL <prussell@e4r5p5.wtc.co.za>
2017-09-07 15:08:47 +0200

    added read and repaired resources assembler

--- asm/srcs/check.c
+++ asm/srcs/check.c
@@ -6,7 +6,7 @@
 /*   By: prussell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/28 09:49:02 by prussell          #+#    #+#             */
-/*   Updated: 2017/09/07 10:08:50 by prussell         ###   ########.fr       */
+/*   Updated: 2017/09/07 15:05:29 by prussell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ int		is_label_address(char *line)
 			return (0);
 		i++;
 	}
-	if (line[i] == LABEL_CHAR && i > 0)
+	if (line[i - 1] == LABEL_CHAR && i > 0)
 		return (1);
 	return (0);
 }
@@ -51,9 +51,11 @@ int		is_label_param(char *line)
 {
 	int	i;
 
-	i = 1;
-	if (line[0] != LABEL_CHAR)
+	i = 0;
+	if (ft_charstr(line, LABEL_CHAR) == NULL)
 		return (0);
+	if (line[i] = DIRECT_CHAR || line [i] == 'r')
+		i++;
 	while (line[i])
 	{
 		if (ft_charstr(LABEL_CHARS, line[i]) == NULL)
@@ -70,14 +72,16 @@ int		is_param(char *line)
 	ret = 0;
 	if (is_label_param(line))
 		return (1);
-	else if (*line == DIRECT_CHAR)
-		ret = DIR_CODE;
-	else if (*line == 'r')
-		ret = 1;
-	else if (ft_isdigits(line))
-		return (1);
-	else if (ret && ft_isdigits(++line))
-		return (ret);
+	while (*line)
+	{
+		else if (*line != DIRECT_CHAR || *line != 'r' || *line != LABEL_CHAR)
+		else if (*line == 'r')
+			ret = 1;
+		else if (ft_number(line))
+			return (1);
+		else if (ret && ft_isdigits(++line))
+			return (ret);
+	}
 	return (0);
 }
 
---
+++ asm/srcs/read.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   read.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: prussell <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/28 09:52:40 by prussell          #+#    #+#             */
+/*   Updated: 2017/09/07 15:05:19 by prussell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "asm.h"
+
+int		extract_data_from_line(char **split, t_src_line *lines)
+{
+	int		label_found;
+	int		op_code_found;
+	int		params_found; 
+	int		i;
+
+	label_found = 0;
+	op_code_found = 0;
+	params_found = 0;
+	i = 0;
+	while (*split)
+	{
+		if (is_comment(*split))
+			break ;
+		else if (is_label_address(*split) && (label_found = 1))
+		{
+//			printf("label found %s\n", *split);
+			lines->label = ft_strdup(*split);
+		}
+		else if (is_opcode(*split) && (op_code_found = 1))
+		{
+//			printf("opcode found %s\n", *split);
+			lines->opcode = is_opcode(*split);
+		}
+		else if (is_param(*split) && i < MAX_ARGS_NUMBER && (params_found = 1))
+		{
+			printf("param found %s\n", *split);
+			lines->params[i] = ft_strdup(*split);
+		}
+		split++;
+	}
+	return (1);
+}
+
+int		get_data(t_src_line *lines, int fd)
+{
+	int		i;
+	char	*line;
+	char	**split;
+
+	i = 0;
+	while (i < MAX_LINES && get_next_line(fd, &line) > 0)
+	{
+		if (*line && !is_comment(line) && (split = core_line_split(line)))
+		{
+			int j = 0;
+			while (split[j])
+				printf("%s\n", split[j++]);
+			extract_data_from_line(split, lines + i);
+			ft_matrixdel((void **)split);
+			i++;
+		}
+		ft_strdel(&line);
+	}
+	if (i == MAX_LINES)
+	{
+		ft_putendl_fd("Lines in file exceed MAX_LINES", 2);
+		return (EXIT_FAILURE);
+	}
+	lines[i].data = NULL;
+	return (EXIT_SUCCESS);
+}
+
+void	init_line_struct(t_src_line *lines)
+{
+	int i;
+	int	j;
+
+	i = 0;
+	while (i < MAX_LINES)
+	{
+		lines[i].data = NULL;
+		lines[i].label = NULL;
+		lines[i].acb = 0;
+		j = 0;
+		while (j < MAX_ARGS_NUMBER)
+			lines[i].params[j++] = NULL;
+		lines[i].opcode = 0;
+		lines[i].bytes = 0;
+		i++;
+	}
+}
+
+int		assemble(int fd)
+{
+	t_src_line lines[MAX_LINES];
+
+	init_line_struct(lines);
+	if (get_data(lines, fd) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	print_struct(lines);
+	return (EXIT_SUCCESS);
+}
