322c9aeade57277c510f65e333f51d4d83243d65
Leslie DE JAGER <lde-jage@e4r4p5.wethinkcode.co.za>
2017-09-30 19:01:10 +0200

    aff

--- vm/srcs/operations/aff.c
+++ vm/srcs/operations/aff.c
@@ -6,7 +6,7 @@
 /*   By: lde-jage <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/21 07:32:39 by lde-jage          #+#    #+#             */
-/*   Updated: 2017/09/29 14:16:16 by prussell         ###   ########.fr       */
+/*   Updated: 2017/09/30 19:00:10 by lde-jage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,17 @@ int			op_aff(t_process *p, t_core *arena)
 	int		reg_data;
 
 	acb = data_var((p->pc + 1) % MEM_SIZE, arena, T_REG);
-	if (is_register(acb, 1))
+	if (is_register(acb, 0))
 	{
 		val = data_var((p->pc + 2) % MEM_SIZE, arena, T_REG);
 		if (valid_reg(val))
 		{
 			reg_data = value_from_reg(p->reg[val - 1]);
-			ft_putchar(reg_data % 256);
+			ft_putchar(reg_data & 0XFF);
 			p->pc = (p->pc + 3) % MEM_SIZE;
 			return (1);
 		}
 	}
-	p->pc = (p->pc + 3) % MEM_SIZE;
+	p->pc = pc_counter(p->pc + 1, acb, 0);
 	return (0);
 }
