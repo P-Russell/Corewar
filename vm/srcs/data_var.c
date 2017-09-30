653cf148bdae0252edb9c9ee5d634df003f980e2
P-Russell <pdrussell011@gmail.com>
2017-09-30 16:54:27 +0200

    data var

--- vm/srcs/data_var.c
+++ vm/srcs/data_var.c
@@ -6,23 +6,19 @@
 /*   By: lde-jage <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/27 07:57:54 by lde-jage          #+#    #+#             */
-/*   Updated: 2017/09/30 16:17:34 by prussell         ###   ########.fr       */
+/*   Updated: 2017/09/30 16:53:27 by prussell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 
-static int32_t	get_val(int pc, t_core *arena)
+static int16_t	get_val(int pc, t_core *arena)
 {
-	int32_t	ans;
+	int16_t	ans;
 
-	ans = arena[pc].raw & 0xFF;
+	ans = arena[pc].raw;
 	ans = ans << 8;
-	ans = ans + arena[(pc + 1) % MEM_SIZE].raw & 0xFF;
-	ans = ans << 8;
-	ans = ans + arena[(pc + 2) % MEM_SIZE].raw & 0xFF;
-	ans = ans << 8;
-	ans = ans + arena[(pc + 3) % MEM_SIZE].raw & 0xFF;
+	ans = ans + arena[(pc + 1) % MEM_SIZE].raw;
 	return (ans);
 }
 
@@ -32,20 +28,20 @@ int		data_var(int pc, t_core *arena, size_t s)
 	int		ans;
 
 	ans = 0;
-	if (s == 1)
+	if (s == T_REG)
 	{
 		var.byte_1 = arena[pc].raw;
 		ans = var.byte_1;
 	}
-	else if (s == 4)
+	else if (s == IND_SIZE)
 	{
-		var.byte_4 = get_val(pc, arena);
-		ans = var.byte_4;
-	}
-	else if (s == 2)
-	{
-		var.byte_2 = value_from_core(arena, pc, (int)s);
+		var.byte_2 = get_val(pc, arena);
 		ans = var.byte_2;
+	}
+	else if (s == REG_SIZE)
+	{
+		var.byte_4 = value_from_core(arena, pc, (int)s);
+		ans = var.byte_4;
 	}
 	return (ans);
 }
