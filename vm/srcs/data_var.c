a63643f49e05a1382b006e875e6da8acc7c1a13d
Leslie DE JAGER <lde-jage@e4r4p5.wethinkcode.co.za>
2017-09-30 16:09:11 +0200

    data_var

--- vm/srcs/data_var.c
+++ vm/srcs/data_var.c
@@ -6,19 +6,23 @@
 /*   By: lde-jage <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/27 07:57:54 by lde-jage          #+#    #+#             */
-/*   Updated: 2017/09/30 11:40:43 by lde-jage         ###   ########.fr       */
+/*   Updated: 2017/09/30 16:08:00 by lde-jage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 
-static int16_t	get_val(int pc, t_core *arena)
+static int32_t	get_val(int pc, t_core *arena)
 {
-	int16_t	ans;
+	int32_t	ans;
 
 	ans = arena[pc].raw & 0xFF;
 	ans = ans << 8;
-	ans = ans + arena[(pc + 1) % MEM_SIZE].raw;
+	ans = ans + arena[(pc + 1) % MEM_SIZE].raw & 0xFF;
+	ans = ans << 8;
+	ans = ans + arena[(pc + 2) % MEM_SIZE].raw & 0xFF;
+	ans = ans << 8;
+	ans = ans + arena[(pc + 3) % MEM_SIZE].raw & 0xFF;
 	return (ans);
 }
 
@@ -33,15 +37,15 @@ int		data_var(int pc, t_core *arena, size_t s)
 		var.byte_1 = arena[pc].raw;
 		ans = var.byte_1;
 	}
-	else if (s == 2)
-	{
-		var.byte_2 = get_val(pc, arena);
-		ans = var.byte_2;
-	}
 	else if (s == 4)
 	{
-		var.byte_4 = value_from_core(arena, pc, (int)s);
+		var.byte_4 = get_val(pc, arena);
 		ans = var.byte_4;
+	}
+	else if (s == 2)
+	{
+		var.byte_2 = value_from_core(arena, pc, (int)s);
+		ans = var.byte_2;
 	}
 	return (ans);
 }
